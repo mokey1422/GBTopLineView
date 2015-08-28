555395170048e291e61998a2de87065575c216d4
mokey1422 <15253503932@163.com>
2015-08-28 16:30:39 +0800

    zahngguobing

---
+++ 仿淘宝头条轮播/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  仿淘宝头条轮播
+//
+//  Created by 张国兵 on 15/8/28.
+//  Copyright (c) 2015年 zhangguobing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
